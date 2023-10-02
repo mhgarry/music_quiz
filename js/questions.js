e08de35d9f47b1e9ee2ef18d29ee953700a8cebc
Matthew Garry <mhgarry92@gmail.com>
2023-10-02 05:04:15 -0400

    start of quiz array

---
+++ js/questions.js
@@ -0,0 +1,27 @@
+const questions = [
+  // question  1
+  {
+    question: "What Washington D.C. band in 1985 was recognized as the first emo band?",
+    choiceA: "Rites of Spring",
+    choiceB: "Sunny Day Real Estate",
+    choiceC: "The Jazz June",
+    choiceD: "Embrace",
+  },
+  // question 2
+  {
+    question: "What is the name of the band that released the album 'Unkwon Pleasures' in 1979?",
+    choiceA: "Wire",
+    choiceB: "Gang of Four",
+    choiceC: "The Cure",
+    choiceD: "Joy Division",
+  },
+  // question 3
+  {
+    question: "Who is the lead singer of the band 'The Smiths'?",
+    choiceA: "Todd Rundgren",
+    choiceB: "Henry Rollins",
+    choiceC: "Morrissey",
+    choiceD: "Robert Smith",
+  },
+
+]
