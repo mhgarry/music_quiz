bfa6cb0b1dc41a497c5df5e8daf5f32285000e6e
Matthew Garry <mhgarry92@gmail.com>
2023-10-03 20:11:21 -0400

    start submit functionality

--- js/index.js
+++ js/index.js
@@ -12,11 +12,25 @@ const choiceD = document.querySelector('.D');
 const submitBtn = document.querySelector('.submit-btn');
 const nextBtn = document.querySelector('.next-btn');
 
-// current question index
-let currentQuestion = 0;
-// current time
-// current score
-let score = 0;
+//question index for array
+const currentQuestionIndex = 0;
+
+
+// functoin to create title and choices display on quiz
+// Function to create title and choices display on quiz
+const showQuestions = () => {
+  // set the question to be displayed using question index
+  const currentQuestionDisplay = questions[currentQuestionIndex];
+  // show question title
+  questionTitles.textContent = currentQuestionDisplay.question;
+  // show answer choices
+  choiceA.textContent = currentQuestionDisplay.answers.A;
+  choiceB.textContent = currentQuestionDisplay.answers.B;
+  choiceC.textContent = currentQuestionDisplay.answers.C;
+  choiceD.textContent = currentQuestionDisplay.answers.D;
+};
+
+// add functionality to submit button
 
 // function to start the timer when the quiz and each individual question starts
 const startTimer = () => {
@@ -29,20 +43,28 @@ const startTimer = () => {
       clearInterval(timer);
 
   }}, 1000) //timer points to setInterval function which counts time variable down from 10
-  
-  }
+};
+
+// submit answer and add score to total score
 
 
 //start quiz and hide intro screen and show quiz screen
 const quizStart = () => {
   startBtn.classList.add('hidden');
   introText.classList.add('hidden');
-  theQuiz.classList.remove('hidden');
-  
-  startTimer();
+  theQuiz.classList.remove('hidden'); 
 };
-
-
+// initiate quiz and start functions
+const init = () => {
+  quizStart();
+  startTimer();
+ showQuestions();
+};
+//event listener attached to the start button that triggers the init function
 startBtn.addEventListener('click', () => {
-  quizStart()
+  init()
 });
+//event listener attached to submit button 
+// submitBtn.addEventListener('click', () => {
+//   
+// })
--- js/questions.js
+++ js/questions.js
@@ -2,169 +2,211 @@ const questions = [
   // question  1
   {
     question: "What Washington D.C. band in 1985 was recognized as the first emo band?",
-    choiceA: "Rites of Spring",
-    choiceB: "Sunny Day Real Estate",
-    choiceC: "The Jazz June",
-    choiceD: "Embrace",
+    answers: { 
+    A: "Rites of Spring",
+    B: "Sunny Day Real Estate",
+    C: "The Jazz June",
+    D: "Embrace",
+    },
   },
   // question 2
   {
     question: "What is the name of the band that released the album 'Unkwon Pleasures' in 1979?",
-    choiceA: "Wire",
-    choiceB: "Gang of Four",
-    choiceC: "The Cure",
-    choiceD: "Joy Division",
+    answers: {
+    A: "Wire",
+    B: "Gang of Four",
+    C: "The Cure",
+    D: "Joy Division",
+    },
   },
   // question 3
   {
     question: "Who is the lead singer of the band 'The Smiths'?",
-    choiceA: "Todd Rundgren",
-    choiceB: "Henry Rollins",
-    choiceC: "Morrissey",
-    choiceD: "Robert Smith",
+    answers: {
+    A: "Todd Rundgren",
+    B: "Henry Rollins",
+    C: "Morrissey",
+    D: "Robert Smith",
+    },
   },
   // question 4
   {
     question: "Which rapper released the album 'Liquid Swords' in 1995?",
-    choiceA: "Biggie Smalls",
-    choiceB: "Andre 3000",
-    ChoiceC: "GZA",
-    choiceD: "Nas",
+    answers: {
+    A: "Biggie Smalls",
+    B: "Andre 3000",
+    C: "GZA",
+    D: "Nas",
+    },
   },
   // question 5
   {
     question: "What was the band Jawbreaker's second studio album released in 1992?",
-    choiceA: "Dear You",
-    choiceB: "Bivouac",
-    choiceC: "24 Hour Revenge Therapy",
-    choiceD: "Unfun",
+    answers: {
+    A: "Dear You",
+    B: "Bivouac",
+    C: "24 Hour Revenge Therapy",
+    D: "Unfun",
+    },
   },
   //question 6
   {
     question: "What artist is largely credited with the creation of the movement 'Straight Edge'?",
-    choiceA: "Ian MacKaye",
-    choiceB: "Henry Rollins",
-    choiceC: "Brian Wilson",
-    choiceD: "Ian Svenonius",
+    answers: {
+    A: "Ian MacKaye",
+    B: "Henry Rollins",
+    C: "Brian Wilson",
+    D: "Ian Svenonius",
+    },
   },
   //question 7 
   {
     question: "Who is the lead singer of the band 'Led Zeppelin'?",
-    choiceA: "Mick Jagger",
-    choiceB: "Robert Plant",
-    choiceC: "Jim Morrison",
-    choiceD: "Thom Yorke",
+    answers: {
+    A: "Mick Jagger",
+    B: "Robert Plant",
+    C: "Jim Morrison",
+    D: "Thom Yorke",
+    }
   },
   // question 8
   {
     question: "What genre is considered the precursor of Dubstep?",
-    choiceA: "Trance",
-    choiceB: "Big Beat",
-    choiceC: "Drum and Bass",
-    choiceD: "Grime",
+    answers: {
+    A: "Trance",
+    B: "Big Beat",
+    C: "Drum and Bass",
+    D: "Grime",
+    },
   },
   //question 9
   {
     question: "What historical figure heavily influenced the album 'In the Aeroplane Over the Sea' by Neutral Milk Hotel?",
-    choiceA: "Albert Einstein",
-    choiceB: "Anne Frank",
-    choiceC: "Emma Goldman",
-    choiceD: "Sigmund Freud",
+    answers: {
+    A: "Albert Einstein",
+    B: "Anne Frank",
+    C: "Emma Goldman",
+    D: "Sigmund Freud",
+    },
   },
   //question 10
   {
     question: "What is the name of the band that released the album 'The Devil and God are Raging Inside Me' in 2006?",
-    choiceA: "Brand New",
-    choiceB: "Taking Back Sunday",
-    choiceC: "The Get Up Kids",
-    choiceD: "Thursday",
+    answers: {
+    A: "Brand New",
+    B: "Taking Back Sunday",
+    C: "The Get Up Kids",
+    D: "Thursday",
+    },
   },
   // question 11
   {
     question: "How many studio albums did the band 'Nirvana' release?",
-    choiceA: "1",
-    choiceB: "4",
-    choiceC: "3",
-    choiceD: "2",
+    answers: {
+    A: "1",
+    B: "4",
+    C: "3",
+    D: "2",
+    },
   },
   // question 12
   {
     question: "What is the origin of the name of the musical genre 'Shoegaze'?",
-    choiceA: "Artists would stare at their pedal boards while playing live shows",
-    choiceB: "Shiny shoes were a staple of the fashion of the genre",
-    choiceC: "The genre was created by a band called 'Low Gaze'",
-    choiceD: "The genre was created by a band called 'My Bloody Valentine'",
+    answers: {
+    A: "Artists would stare at their pedal boards while playing live shows",
+    B: "Shiny shoes were a staple of the fashion of the genre",
+    C: "The genre was created by a band called 'Low Gaze'",
+    D: "The genre was created by a band called 'My Bloody Valentine'",
+    },
   },
   // question 13
   {
     question: "What revolutionary ideas did the genre Jazz introduce to the world of music?",
-    choiceA: "Improvisation",
-    choiceB: "Syncopation",
-    choiceC: "Both A and B",
-    choiceD: "The pentatonic scale",
+    answers: {
+    A: "Improvisation",
+    B: "Syncopation",
+    C: "Both A and B",
+    D: "The pentatonic scale",
+    },
   },
   //question 14
   {
     question: "What band embodies the slacker indie vibe of the 90s?",
-    choiceA: "Pavement",
-    choiceB: "Sonic Youth",
-    choiceC: "The Pixies",
-    choiceD: "Dinosaur Jr.",
+    answers: {
+    A: "Pavement",
+    B: "Sonic Youth",
+    C: "The Pixies",
+    D: "Dinosaur Jr.",
+    },
   },
   {
     question: "Where is Bruce Springstean's self proclaimed favorite place to play in his home state of New Jersey?",
-    choiceA: "The Stone Pony",
-    choiceB: "Madison Square Garden",
-    choiceC: "PNC Arena",
-    choiceD: "The Meadowlands"
+    answers: {
+    A: "The Stone Pony",
+    B: "Madison Square Garden",
+    C: "PNC Arena",
+    D: "The Meadowlands"
+    },
   },
   // question 15
   {
     question: "What city in central New Jersey is knwon for passionate basement shows in the late '90s and early '00s",
-    choiceA: "Sayerville",
-    choiceB: "New Brunswick",
-    choiceC: "Princeton",
-    choiceD: "Newark"
+    answers: {
+    A: "Sayerville",
+    B: "New Brunswick",
+    C: "Princeton",
+    D: "Newark"
+    },
   },
   //question 16
   {
     question: "What famous blues guitairst was rumored to sell his soul to the devil for his guitar skills?",
-    choiceA: "Jimi Hendrix",
-    choiceB: "Lonnie Johnson",
-    choiceC: "Robert Johnson",
-    choiceD: "Big Bill Broozy"
+    answers: {
+    A: "Jimi Hendrix",
+    B: "Lonnie Johnson",
+    C: "Robert Johnson",
+    D: "Big Bill Broozy"
+    },
   },
   // question 17
   {
     question: "Syncopation is a concept heavily used in Jazz. What does it mean?",
-    choiceA: "Stressing or highlighting the 'off' or 'weak' beat in a song",
-    choiceB: "A classic 4/4 beat",
-    choiceC: "When a singer purposely sings off key",
-    choiceD: "A layered guitar tone"
+    answers: {
+    A: "Stressing or highlighting the 'off' or 'weak' beat in a song",
+    B: "A classic 4/4 beat",
+    C: "When a singer purposely sings off key",
+    D: "A layered guitar tone"
+    },
 
   },
   // question 18
   {
     question: "What musical genre did bands like Fugazi, Drive Like Jehu, and At The Drive In pioneer",
-    choiceA: "Post-Rock",
-    choiceB: "Post-Hardcore",
-    choiceC: "Post-Punk",
-    choiceD: "Metalcore"
+    answers: {
+    A: "Post-Rock",
+    B: "Post-Hardcore",
+    C: "Post-Punk",
+    D: "Metalcore"
+    },
   },
   // question 19
   {
     question: "What genre of electronic music shaped the early rave scene in England and Germany?",
-    choiceA: "Trance",
-    choiceB: "Big Room House",
-    choiceC: "Drum and Bass",
-    choiceD: "Techno"
+    answers: {
+    A: "Trance",
+    B: "Big Room House",
+    C: "Drum and Bass",
+    D: "Techno"
+    },
   },
   // question 20
   {
     question: "Where did the wildly influential proto-punk band Velvet Underground play their first show",
-    choiceA: "CBGBs",
-    choiceB: "Andy Warhol's Factory",
-    choiceC: "Summit High School in Summit, New Jersey",
-    choiceD: "The Funhouse"
+    answers: {
+    A: "CBGBs",
+    B: "Andy Warhol's Factory",
+    C: "Summit High School in Summit, New Jersey",
+    D: "The Funhouse"
+    },
   },
 ];
