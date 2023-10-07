56f79478936a70875e8df376da7b2bc9bb1b848a
Matthew Garry <mhgarry92@gmail.com>
2023-10-07 15:18:03 -0400

    fix timer function

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
-// constant variables to be used in quiz application
+// constant variables to be used in the quiz application
 const startBtn = document.querySelector('.start-btn');
 const timeDisplay = document.querySelector('.timer');
 const questionTitles = document.querySelector('.question-title');
 const theQuiz = document.querySelector('.quiz-container')
 const questionChoices = document.querySelector('.choices');
 
-
-//question index for array
+// question index for array
 let currentQuestionIndex = 0;
 // starting score
 let score = 0;
+let timer;  // Declare timer variable outside the startTimer function
 
 // initiate quiz and start functions
 const init = () => {
   quizStart();
-  startTimer();
   showQuestions();
+  startTimer();
 };
 
-startBtn.addEventListener('click', init); //runs init function on click
+startBtn.addEventListener('click', init); // runs init function on click
 
-//start quiz and hide intro screen and show quiz screen
+// start quiz and hide intro screen and show quiz screen
 const quizStart = () => {
   startBtn.classList.add('hidden');
   theQuiz.classList.remove('hidden'); 
@@ -29,40 +29,46 @@ const quizStart = () => {
 // function to start the timer when the quiz and each individual question starts
 const startTimer = () => {
   let time = 10;
-  
-  let timer = setInterval(() => {
+  timer = setInterval(() => {
     time--;
     timeDisplay.innerText = time;
     if (time <= 0) {
       clearInterval(timer);
-
-  }}, 1000) //timer points to setInterval function which counts time variable down from 10
+      showNextQuestion();
+    }
+  }, 1000); // timer points to setInterval function which counts time variable down from 10
 };
 
 // function to show questions and choices 
 const showQuestions = () => {
-  //sets current question title
+  // sets current question title
   const currentQuestion = questions[currentQuestionIndex].question;
-  //sets choice variables
+  // sets choice variables
   const A = questions[currentQuestionIndex].answers.A;
   const B = questions[currentQuestionIndex].answers.B;
   const C = questions[currentQuestionIndex].answers.C;
   const D = questions[currentQuestionIndex].answers.D;
-  //sets current question title and choices
+  // sets current question title and choices
   questionTitles.innerText = currentQuestion;
   questionChoices.innerHTML =   
- `<button>${A}</button>
-  <button>${B}</button>
-  <button>${C}</button>
-  <button>${D}</button>`;
+   `<button>${A}</button>
+    <button>${B}</button>
+    <button>${C}</button>
+    <button>${D}</button>`;
   const correctAnswer = questions[currentQuestionIndex].answer;
-  console.log(correctAnswer)
+  console.log(correctAnswer);
 };
 
-// function to check if answer is correct
+// function to check if the answer is correct
 const showNextQuestion = () => {
   currentQuestionIndex++;
-  showQuestions();
+  if (currentQuestionIndex < questions.length) {
+    clearInterval(timer);  // Clear the previous interval
+    showQuestions();
+    startTimer();
+  } else {
+    // Quiz is over, do something (e.g., display final score)
+  }
 };
 
 const checkAnswer = (correctAnswer, userAnswer) => {
@@ -71,7 +77,7 @@ const checkAnswer = (correctAnswer, userAnswer) => {
     console.log(score);
     showNextQuestion();
   }
-} 
+};
 
 // event listener for answer buttons
 questionChoices.addEventListener('click', (event) => {
@@ -79,14 +85,3 @@ questionChoices.addEventListener('click', (event) => {
   const correctAnswer = questions[currentQuestionIndex].answer;
   checkAnswer(correctAnswer, userAnswer);
 });
-  
-  // // loops through questions array and displays current question and choices
-  // for (let i = 0; i < questions.length; i++) {}
-  //   questionTitles.innerText = currentQuestion;
-  //   questionChoices.innerHTML = `<button>${A}</button>
-  //   <button>${B}</button>
-  //   <button>${C}</button>
-  //   <button>${D}</button>`;
-
-
-
