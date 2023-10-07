0203632d11fb36d1bcb50bad182bb37987654e17
Matthew Garry <mhgarry92@gmail.com>
2023-10-07 19:11:45 -0400

    break questions into smaller arrays

--- js/questions.js
+++ js/questions.js
@@ -1,233 +1,291 @@
 const questions = [
   // question  1
-  {
-    question: "What Washington D.C. band in 1985 was recognized as the first emo band?",
-    answers: { 
-    A: "Rites of Spring",
-    B: "Sunny Day Real Estate",
-    C: "The Jazz June",
-    D: "Embrace",
+  [
+    {
+      question:
+        "What Washington D.C. band in 1985 was recognized as the first emo band?",
+      answers: {
+        A: "Rites of Spring",
+        B: "Sunny Day Real Estate",
+        C: "The Jazz June",
+        D: "Embrace",
+      },
+      answer: "Rites of Spring",
     },
-    answer: "Rites of Spring",
-  },
+  ],
   // question 2
-  {
-    question: "What is the name of the band that released the album 'Unkwon Pleasures' in 1979?",
-    answers: {
-    A: "Wire",
-    B: "Gang of Four",
-    C: "The Cure",
-    D: "Joy Division",
+  [
+    {
+      question:
+        "What is the name of the band that released the album 'Unkwon Pleasures' in 1979?",
+      answers: {
+        A: "Wire",
+        B: "Gang of Four",
+        C: "The Cure",
+        D: "Joy Division",
+      },
+      answer: "Joy Division",
     },
-    answer: "Joy Division",
-  },
+  ],
   // question 3
-  {
-    question: "Who is the lead singer of the band 'The Smiths'?",
-    answers: {
-    A: "Todd Rundgren",
-    B: "Henry Rollins",
-    C: "Morrissey",
-    D: "Robert Smith",
+  [
+    {
+      question: "Who is the lead singer of the band 'The Smiths'?",
+      answers: {
+        A: "Todd Rundgren",
+        B: "Henry Rollins",
+        C: "Morrissey",
+        D: "Robert Smith",
+      },
+      answer: "Morrissey",
     },
-    answer: "Morrissey"
-  },
+  ],
   // question 4
-  {
-    question: "Which rapper released the album 'Liquid Swords' in 1995?",
-    answers: {
-    A: "Biggie Smalls",
-    B: "Andre 3000",
-    C: "GZA",
-    D: "Nas",
+  [
+    {
+      question: "Which rapper released the album 'Liquid Swords' in 1995?",
+      answers: {
+        A: "Biggie Smalls",
+        B: "Andre 3000",
+        C: "GZA",
+        D: "Nas",
+      },
+      answer: "GZA",
     },
-    answer: "GZA",
-  },
+  ],
   // question 5
-  {
-    question: "What was the band Jawbreaker's second studio album released in 1991?",
-    answers: {
-    A: "Dear You",
-    B: "Bivouac",
-    C: "24 Hour Revenge Therapy",
-    D: "Unfun",
+  [
+    {
+      question:
+        "What was the band Jawbreaker's second studio album released in 1991?",
+      answers: {
+        A: "Dear You",
+        B: "Bivouac",
+        C: "24 Hour Revenge Therapy",
+        D: "Unfun",
+      },
+      answer: "Bivouac",
     },
-    answer: "Bivouac",
-  },
+  ],
   //question 6
-  {
-    question: "What artist is largely credited with the creation of the movement 'Straight Edge'?",
-    answers: {
-    A: "Ian MacKaye",
-    B: "Henry Rollins",
-    C: "Brian Wilson",
-    D: "Ian Svenonius",
+  [
+    {
+      question:
+        "What artist is largely credited with the creation of the movement 'Straight Edge'?",
+      answers: {
+        A: "Ian MacKaye",
+        B: "Henry Rollins",
+        C: "Brian Wilson",
+        D: "Ian Svenonius",
+      },
+      answer: "Ian MacKaye",
     },
-    answer: "Ian MacKaye",
-  },
-  //question 7 
-  {
-    question: "Who is the lead singer of the band 'Led Zeppelin'?",
-    answers: {
-    A: "Mick Jagger",
-    B: "Robert Plant",
-    C: "Jim Morrison",
-    D: "Thom Yorke",
+  ],
+  //question 7
+  [
+    {
+      question: "Who is the lead singer of the band 'Led Zeppelin'?",
+      answers: {
+        A: "Mick Jagger",
+        B: "Robert Plant",
+        C: "Jim Morrison",
+        D: "Thom Yorke",
+      },
+      answer: "Robert Plant",
     },
-    answer: "Robert Plant",
-  },
+  ],
   // question 8
-  {
-    question: "What genre is considered the precursor of Dubstep?",
-    answers: {
-    A: "Trance",
-    B: "Big Beat",
-    C: "Drum and Bass",
-    D: "Grime",
+  [
+    {
+      question: "What genre is considered the precursor of Dubstep?",
+      answers: {
+        A: "Trance",
+        B: "Big Beat",
+        C: "Drum and Bass",
+        D: "Grime",
+      },
+      answer: "Drum and Bass",
     },
-    answer: "Drum and Bass",
-  },
+  ],
   //question 9
-  {
-    question: "What historical figure heavily influenced the album 'In the Aeroplane Over the Sea' by Neutral Milk Hotel?",
-    answers: {
-    A: "Albert Einstein",
-    B: "Anne Frank",
-    C: "Emma Goldman",
-    D: "Sigmund Freud",
+  [
+    {
+      question:
+        "What historical figure heavily influenced the album 'In the Aeroplane Over the Sea' by Neutral Milk Hotel?",
+      answers: {
+        A: "Albert Einstein",
+        B: "Anne Frank",
+        C: "Emma Goldman",
+        D: "Sigmund Freud",
+      },
+      answer: "Anne Frank",
     },
-    answer: "Anne Frank",
-  },
+  ],
   //question 10
-  {
-    question: "What is the name of the band that released the album 'The Devil and God are Raging Inside Me' in 2006?",
-    answers: {
-    A: "Brand New",
-    B: "Taking Back Sunday",
-    C: "The Get Up Kids",
-    D: "Thursday",
+  [
+    {
+      question:
+        "What is the name of the band that released the album 'The Devil and God are Raging Inside Me' in 2006?",
+      answers: {
+        A: "Brand New",
+        B: "Taking Back Sunday",
+        C: "The Get Up Kids",
+        D: "Thursday",
+      },
+      answer: "Brand New",
     },
-    answer: "Brand New",
-  },
+  ],
   // question 11
-  {
-    question: "How many studio albums did the band 'Nirvana' release?",
-    answers: {
-    A: "1",
-    B: "4",
-    C: "3",
-    D: "2",
+  [
+    {
+      question: "How many studio albums did the band 'Nirvana' release?",
+      answers: {
+        A: "1",
+        B: "4",
+        C: "3",
+        D: "2",
+      },
+      answer: "2",
     },
-    answer: "2",
-  },
+  ],
   // question 12
-  {
-    question: "What is the origin of the name of the musical genre 'Shoegaze'?",
-    answers: {
-    A: "Artists would stare at their pedal boards while playing live shows",
-    B: "Shiny shoes were a staple of the fashion of the genre",
-    C: "The genre was created by a band called 'Low Gaze'",
-    D: "The genre was created by a band called 'My Bloody Valentine'",
+  [
+    {
+      question:
+        "What is the origin of the name of the musical genre 'Shoegaze'?",
+      answers: {
+        A: "Artists would stare at their pedal boards while playing live shows",
+        B: "Shiny shoes were a staple of the fashion of the genre",
+        C: "The genre was created by a band called 'Low Gaze'",
+        D: "The genre was created by a band called 'My Bloody Valentine'",
+      },
+      answer:
+        "Artists would stare at their pedal boards while playing live shows",
     },
-    answer: "Artists would stare at their pedal boards while playing live shows",
-  },
+  ],
   // question 13
-  {
-    question: "What revolutionary ideas did the genre Jazz introduce to the world of music?",
-    answers: {
-    A: "Improvisation",
-    B: "Syncopation",
-    C: "Both A and B",
-    D: "The pentatonic scale",
+  [
+    {
+      question:
+        "What revolutionary ideas did the genre Jazz introduce to the world of music?",
+      answers: {
+        A: "Improvisation",
+        B: "Syncopation",
+        C: "Both A and B",
+        D: "The pentatonic scale",
+      },
+      answer: "Both A and B",
     },
-    answer: "Both A and B",
-  },
+  ],
   //question 14
-  {
-    question: "What band embodies the slacker indie vibe of the 90s?",
-    answers: {
-    A: "Pavement",
-    B: "Sonic Youth",
-    C: "The Pixies",
-    D: "Dinosaur Jr.",
+  [
+    {
+      question: "What band embodies the slacker indie vibe of the 90s?",
+      answers: {
+        A: "Pavement",
+        B: "Sonic Youth",
+        C: "The Pixies",
+        D: "Dinosaur Jr.",
+      },
+      answer: "Pavement",
     },
-    answer: "Pavement",
-  },
+  ],
   // question 15
-  {
-    question: "Where is Bruce Springstean's self proclaimed favorite place to play in his home state of New Jersey?",
-    answers: {
-    A: "The Stone Pony",
-    B: "Madison Square Garden",
-    C: "PNC Arena",
-    D: "The Meadowlands",
+  [
+    {
+      question:
+        "Where is Bruce Springstean's self proclaimed favorite place to play in his home state of New Jersey?",
+      answers: {
+        A: "The Stone Pony",
+        B: "Madison Square Garden",
+        C: "PNC Arena",
+        D: "The Meadowlands",
+      },
+      answer: "The Stone Pony",
     },
-    answer: "The Stone Pony",
-  },
+  ],
   // question 16
-  {
-    question: "What city in central New Jersey is knwon for passionate basement shows in the late '90s and early '00s",
-    answers: {
-    A: "Sayerville",
-    B: "New Brunswick",
-    C: "Princeton",
-    D: "Newark",
+  [
+    {
+      question:
+        "What city in central New Jersey is knwon for passionate basement shows in the late '90s and early '00s",
+      answers: {
+        A: "Sayerville",
+        B: "New Brunswick",
+        C: "Princeton",
+        D: "Newark",
+      },
+      answer: "New Brunswick",
     },
-    answer: "New Brunswick",
-  },
+  ],
   //question 16
-  {
-    question: "What famous blues guitairst was rumored to sell his soul to the devil for his guitar skills?",
-    answers: {
-    A: "Jimi Hendrix",
-    B: "Lonnie Johnson",
-    C: "Robert Johnson",
-    D: "Big Bill Broozy",
+  [
+    {
+      question:
+        "What famous blues guitairst was rumored to sell his soul to the devil for his guitar skills?",
+      answers: {
+        A: "Jimi Hendrix",
+        B: "Lonnie Johnson",
+        C: "Robert Johnson",
+        D: "Big Bill Broozy",
+      },
+      answer: "Robert Johnson",
     },
-    answer: "Robert Johnson",
-  },
+  ],
   // question 17
-  {
-    question: "Syncopation is a concept heavily used in Jazz. What does it mean?",
-    answers: {
-    A: "Stressing or highlighting the 'off' or 'weak' beat in a song",
-    B: "A classic 4/4 beat",
-    C: "When a singer purposely sings off key",
-    D: "A layered guitar tone",
+  [
+    {
+      question:
+        "Syncopation is a concept heavily used in Jazz. What does it mean?",
+      answers: {
+        A: "Stressing or highlighting the 'off' or 'weak' beat in a song",
+        B: "A classic 4/4 beat",
+        C: "When a singer purposely sings off key",
+        D: "A layered guitar tone",
+      },
+      answer: "Stressing or highlighting the 'off' or 'weak' beat in a song",
     },
-    answer: "Stressing or highlighting the 'off' or 'weak' beat in a song",
-  },
+  ],
   // question 18
-  {
-    question: "What musical genre did bands like Fugazi, Drive Like Jehu, and At The Drive In pioneer",
-    answers: {
-    A: "Post-Rock",
-    B: "Post-Hardcore",
-    C: "Post-Punk",
-    D: "Metalcore",
+  [
+    {
+      question:
+        "What musical genre did bands like Fugazi, Drive Like Jehu, and At The Drive In pioneer",
+      answers: {
+        A: "Post-Rock",
+        B: "Post-Hardcore",
+        C: "Post-Punk",
+        D: "Metalcore",
+      },
+      answer: "Post-Hardcore",
     },
-    answer: "Post-Hardcore",
-  },
+  ],
   // question 19
-  {
-    question: "What genre of electronic music shaped the early rave scene in England and Germany?",
-    answers: {
-    A: "Trance",
-    B: "Big Room House",
-    C: "Drum and Bass",
-    D: "Techno",
+  [
+    {
+      question:
+        "What genre of electronic music shaped the early rave scene in England and Germany?",
+      answers: {
+        A: "Trance",
+        B: "Big Room House",
+        C: "Drum and Bass",
+        D: "Techno",
+      },
+      answer: "Trance",
     },
-    answer: "Trance",
-  },
+  ],
   // question 20
-  {
-    question: "Where did the wildly influential proto-punk band Velvet Underground play their first show",
-    answers: {
-    A: "CBGBs",
-    B: "Andy Warhol's Factory",
-    C: "Summit High School in Summit, New Jersey",
-    D: "The Funhouse",
+  [
+    {
+      question:
+        "Where did the wildly influential proto-punk band Velvet Underground play their first show",
+      answers: {
+        A: "CBGBs",
+        B: "Andy Warhol's Factory",
+        C: "Summit High School in Summit, New Jersey",
+        D: "The Funhouse",
+      },
+      answer: "Summit High School in Summit, New Jersey",
     },
-    answer: "Summit High School in Summit, New Jersey",
-  },
+  ],
 ];
