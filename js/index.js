5efee4d5c26921599a2c75194d5fcf9c3171ba1c
Matthew Garry <mhgarry92@gmail.com>
2023-10-07 23:31:19 -0400

    added error checking

--- js/index.js
+++ js/index.js
@@ -6,22 +6,29 @@ const theQuiz = document.querySelector('.quiz-container')
 const questionChoices = document.querySelector('.btn');
 const scoreDisplay = document.querySelector('.score');
 const userInput = document.querySelectorAll('.btn');  // selects all buttons  
+const results = document.querySelector('.results-container');
+const endScore = document.querySelector('.final-score');
+const submitScore = document.querySelector('.submit-score');
+const highScores = document.querySelector('.high-score');
+const restart = document.querySelector('.restart-btn');
 // use the Fisher-Yates shuffle algorithm to shuffle the questions array to randomize the question order
-const shuffleIndex = (index) => {
-  for (let i = index.length - 1; i > 0; i--) {
-    const randomIndex = Math.floor(Math.random() * (i + 1));
-    [index[i], index[randomIndex]] = [index[randomIndex], index[i]];
-  }
-}
+
 // question index for array
 let currentQuestionIndex = 0;
 // starting score
 let score = 0;
 let timer;  // Declare timer variable outside the startTimer function
 
+const shuffleIndex = (index) => {
+  for (let i = index.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [index[i], index[randomIndex]] = [index[randomIndex], index[i]];
+  }
+}
+
 // initiate quiz and start functions
 const init = () => {
-  shuffleIndex(questions);
+  shuffleIndex(questions)
   quizStart();
   showQuestions();
   startTimer();
@@ -49,6 +56,7 @@ const startTimer = () => {
 
 // function to show questions and choices 
 const showQuestions = () => {
+  if (questions[currentQuestionIndex]) {
   // sets current question title
   const currentQuestion = questions[currentQuestionIndex].question;
   //sets current question choices
@@ -68,6 +76,9 @@ const showQuestions = () => {
       <button>${D}</button>
     </div>
   `;
+  } else {
+    console.error('No question found for the current index')
+  }
 };
 
 // function to show next question
@@ -80,8 +91,13 @@ const nextQuestion = () => {
 
 // function to check answer
 const checkAnswer = (input, answer) => {
+  
   input = input.target.innerText;
+  if (questions[currentQuestionIndex]) {
   answer = questions[currentQuestionIndex].answer;
+  } else {
+    console.error('No question found')
+  }
   if (input === answer) {
     score++;
     scoreDisplay.innerHTML = `<span>Score:</span>${score}`;
@@ -93,18 +109,7 @@ const checkAnswer = (input, answer) => {
   }
   nextQuestion();
 };
-//save scores to local storage 
-const saveScore = () => {
-  const finalScore = document.querySelector('.final-score').value;
-  const userInitials = document.querySelector('#name').value
-  localStorage.setItem('User Score', finalScore);
-  localStorage.setItem('User Intials', userInitials);
-};
 
-const getScores = () => {
-  const finalScore = localStorage.getItem(finalScore);
-  const userInitials = localStorage.getItem(userInitials);
-};
 
 // event listener for user input
 userInput.forEach((input) => {
