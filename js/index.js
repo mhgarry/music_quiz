8c5754d7bf3e1a5f5fbca00d8a1932164ec59b15
Matthew Garry <mhgarry92@gmail.com>
2023-10-07 22:05:28 -0400

    refactored made inputs functional

--- js/index.js
+++ js/index.js
@@ -3,7 +3,9 @@ const startBtn = document.querySelector('.start-btn');
 const timeDisplay = document.querySelector('.timer');
 const questionTitles = document.querySelector('.question-title');
 const theQuiz = document.querySelector('.quiz-container')
-const questionChoices = document.querySelector('.choices');
+const questionChoices = document.querySelector('.btn');
+const scoreDisplay = document.querySelector('.score');
+const userInput = document.querySelectorAll('.btn');  // selects all buttons  
 
 // question index for array
 let currentQuestionIndex = 0;
@@ -34,7 +36,6 @@ const startTimer = () => {
     timeDisplay.innerText = time;
     if (time <= 0) {
       clearInterval(timer);
-      showNextQuestion();
     }
   }, 1000); // timer points to setInterval function which counts time variable down from 10
 };
@@ -43,54 +44,46 @@ const startTimer = () => {
 const showQuestions = () => {
   // sets current question title
   const currentQuestion = questions[currentQuestionIndex].question;
-  // sets choice variables
-  const A = questions[currentQuestionIndex].answers.A;
-  const B = questions[currentQuestionIndex].answers.B;
-  const C = questions[currentQuestionIndex].answers.C;
-  const D = questions[currentQuestionIndex].answers.D;
-  // sets current question title and choices
+  //sets current question choices
+  const currentAnswers = questions[currentQuestionIndex].answers;
+  const A = currentAnswers.A;
+  const B = currentAnswers.B;
+  const C = currentAnswers.C;
+  const D = currentAnswers.D;
+  // loops through title and choices and displays them
   questionTitles.innerText = currentQuestion;
-  questionChoices.innerHTML =   
-   `<button>${A}</button>
-    <button>${B}</button>
-    <button>${C}</button>
-    <button>${D}</button>`;
-  const correctAnswer = questions[currentQuestionIndex].answer;
-  console.log(correctAnswer);
+  // sets current question title and choices
+  questionChoices.innerHTML = `
+    <div class="choices-container">
+      <button>${A}</button>
+      <button>${B}</button>
+      <button>${C}</button>
+      <button>${D}</button>
+    </div>
+  `;
 };
 
-// function to check if the answer is correct
-const showNextQuestion = () => {
+// function to show next question
+const nextQuestion = () => { 
   currentQuestionIndex++;
-  if (currentQuestionIndex < questions.length) {
-    clearInterval(timer);  // Clear the previous interval
-    showQuestions();
-    startTimer();
-  } else {
-    // Quiz is over, do something (e.g., display final score)
-  }
+  clearInterval(timer);
+  startTimer();
+  showQuestions();
 };
 
-const checkAnswer = (correctAnswer, userAnswer) => {
-  if (correctAnswer === userAnswer) {
+// function to check answer
+const checkAnswer = (input, answer) => {
+  input = input.target.innerText;
+  answer = questions[currentQuestionIndex].answer;
+  if (input === answer) {
     score++;
+    scoreDisplay.innerText = score;
     console.log(score);
-    showNextQuestion();
   }
+  nextQuestion();
 };
 
-// event listener for answer buttons
-questionChoices.addEventListener('click', (event) => {
-  const userAnswer = event.target.innerText;
-  const correctAnswer = questions[currentQuestionIndex].answer;
-  checkAnswer(correctAnswer, userAnswer);
-  endQuiz();
+// event listener for user input
+userInput.forEach((input) => {
+  input.addEventListener('click', checkAnswer);
 });
-
-const endQuiz = () => {
-  if (questions.length === currentQuestionIndex) {
-    clearInterval(timer);
-    theQuiz.classList.add('hidden');
-    console.log('quiz over');
-  } 
-}
